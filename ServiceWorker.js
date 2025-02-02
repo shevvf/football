10ef5d229bacf8cbcfed225bfeed03a6a9b142d3
shevvf <kshevlev@mail.ru>
2025-02-02 17:49:25 +0300

    Update ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -5,29 +5,49 @@ const contentToCache = [
     "Build/Football.data.unityweb",
     "Build/Football.wasm.unityweb",
     "TemplateData/style.css"
-
 ];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
-    
     e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(contentToCache);
     })());
+});
+
+self.addEventListener('activate', function (e) {
+    console.log('[Service Worker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cachedName) {
+                    if (cachedName !== cacheName) {
+                        console.log('[Service Worker] Deleting old cache:', cachedName);
+                        return caches.delete(cachedName);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', function (e) {
     e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
+        try {
+            const response = await fetch(e.request);
+            const cache = await caches.open(cacheName);
+            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+            cache.put(e.request, response.clone());
+            return response;
+        } catch (error) {
+            console.log(`[Service Worker] Fetch failed; falling back to cache: ${e.request.url}`);
+            return caches.match(e.request);
+        }
     })());
+});
+
+self.addEventListener('controllerchange', function () {
+    console.log('[Service Worker] Controller changed, reloading page...');
+    window.location.reload();
 });
